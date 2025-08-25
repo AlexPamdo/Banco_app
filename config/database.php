26aba228b2441278214fd8d9d7f79445c428852c
AlexPamdo <114254269+AlexPamdo@users.noreply.github.com>
2025-08-25 12:05:26 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,20 +1,23 @@
 <?php
 
 class Database {
-    private $host = 'localhost';
-    private $dbname = 'bancalex';
-    private $user = 'root';
-    private $password = '';
+    private $host = 'aws-1-sa-east-1.pooler.supabase.com';
+    private $dbname = 'postgres';
+    private $user = 'postgres.rhwjjuvucewgwhretpbx ';
+    private $password = '123456789Dd*';
+    private $port = 6543;
 
     public $conn;
 
     public function getConnection() {
         if($this->conn === null){
             try{
-                $this->conn = new PDO(
-                    "mysql:host={$this->host};dbname={$this->dbname}",
-                    $this->user,
-                    password: $this->password,
+               $this->conn = new PDO(
+    "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}",
+    $this->user,
+    $this->password
+);
+
                 );
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }catch(PDOException $e){
